b87841b5953022ccf13fd1a3bc1cd2e8a661382f
codename11 <veljkos82@gmail.com>
2018-08-25 07:16:57 +0200

    Moved components to different directory.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import 'jquery-ui-bundle/jquery-ui.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/style.css';
 import './assets/css/animate.min.css';
-import EditorHead from './EditorHead.js';
-import PreviewHead from './PreviewHead.js';
-import BtnEraser from './BtnEraser.js';
-import Editor from './Editor.js';
-import Preview from './Preview.js';
-import Arrow from './Arrow.js';
+import EditorHead from './assets/js/EditorHead.js';
+import PreviewHead from './assets/js/PreviewHead.js';
+import BtnEraser from './assets/js/BtnEraser.js';
+import Editor from './assets/js/Editor.js';
+import Preview from './assets/js/Preview.js';
+import Arrow from './assets/js/Arrow.js';
 
 var placeholder = '# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here\'s some other cool stuff:\n  \nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == \'```\' && lastLine == \'```\') {\n    return multiLineCode;\n  }\n}\n```\n  \nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere\'s also [links](https://www.freecodecamp.com), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | ------------- \nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbererd lists too.\n1. Use just 1s if you want! \n1. But the list goes on...\n- Even if you use dashes or asterisks.\n* And last but not least, let\'s not forget embedded images:\n\n![React Logo w/ Text](https://goo.gl/Umyytc)\n';
 
